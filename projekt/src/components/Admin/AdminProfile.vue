be5c03551901c2bb453b8e770c9805e11f9662c5
Miłosz Kotarba <111527970+miloszkotarba@users.noreply.github.com>
2026-06-17 13:25:30 +0200

    Order Management + Order History Statuses

--- projekt/src/components/Admin/AdminProfile.vue
+++ projekt/src/components/Admin/AdminProfile.vue
@@ -199,6 +199,10 @@ onMounted(loadUserDetails)
               ></span>
               <span class="menu-text">Tag Management</span>
             </li>
+            <li @click="router.push('/admin/order-management')">
+              <span class="icon">📦</span>
+              <span class="menu-text">Order Management</span>
+            </li>
             <li @click="router.push('/admin/user-management')">
               <span class="icon">👥</span>
               <span class="menu-text">User Management</span>
